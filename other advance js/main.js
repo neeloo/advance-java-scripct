0fa87936cae82a44a4543adbd3fbb5d2b38f5e11
Nilu patel <nilusingh945164@gmail.com>
2023-07-29 08:08:55 +0530

    js commit

--- other advance js/main.js
+++ other advance js/main.js
@@ -14,6 +14,7 @@
 
 
 //closure**************
+
 // function outer() {
 //     let count = 0;
 //     function inner() {
@@ -29,6 +30,7 @@
 // fn();
 
 //***************function currying */
+
 // function sum(a, b, c) {
 //     return a + b + c;
 // }
@@ -64,6 +66,7 @@
 //how to determine 'this'///////////////////
 
 //impersonlicit
+
 // const person={
 //     name:'swatantra',
 //     sayMyName:function(){
@@ -73,12 +76,14 @@
 // person.sayMyName();
 
 // // //expersonlicit
+
 // function sayMyName(){
 //     console.log(`my name is  ${this.name}`)
 // }
 // sayMyName.call(person)
 
 // // //new binding
+
 // function person(name){
 //     this.name=name
 // }
@@ -87,35 +92,87 @@
 // console.log(a1.name,a2.name);
 
 // //default binding
+
 // sayMyName();
 
 
 
 //order of personrecendance////////////////
-function person(fname, lname) {
-    this.firstname = fname;
-    this.lastname = lname;
-}
-const person1 = new person('nilu', 'singh')
-const person2 = new person('swatantar', 'verma')
 
-person1.getFullName = function () {
-    return this.firstname + ' ' + this.lastname
-}
-console.log(person1.getFullName());
+// function person(fname, lname) {
+//     this.firstname = fname;
+//     this.lastname = lname;
+// }
+// const person1 = new person('nilu', 'singh')
+// const person2 = new person('swatantar', 'verma')
 
-person2.getFullName = function () {
-    return this.firstname + ' ' + this.lastname
-}
-console.log(person2.getFullName());
+// person1.getFullName = function () {
+//     return this.firstname + ' ' + this.lastname
+// }
+// console.log(person1.getFullName());
 
-   //   prototype******************
-person.prototype.getFullName = function () {
-    return this.firstname + ' ' + this.lastname
-}
-console.log(person1.getFullName());
-console.log(person2.getFullName());
+// person2.getFullName = function () {
+//     return this.firstname + ' ' + this.lastname
+// }
+// console.log(person2.getFullName());
 
+//    //   prototype******************
+// person.prototype.getFullName = function () {
+//     return this.firstname + ' ' + this.lastname
+// }
+// console.log(person1.getFullName());
+// console.log(person2.getFullName());
+
+
+//inheritance in prototype function**********
+
+// function person(fname, lname) {
+//     this.firstname = fname;
+//     this.lastname = lname;
+// }
+// person.prototype.getFullName = function () {
+//     return this.firstname + ' ' + this.lastname
+// }
+// function superHero(fname,lname){
+//     person.call(this,fname,lname)
+//     this.issuperHero=true;
+// }
+// superHero.prototype.fightCrime=function(){
+//     console.log('Fighting crime');
+// }
+// superHero.prototype=Object.create(person.prototype)
+
+// const batman=new superHero('nilu','singh');
+// superHero.prototype.constructor=superHero
+// console.log(batman.getFullName())
+
+
+//use of constructor****************
+
+class person{
+    constructor(fname,lname){
+        this.firstname=fname
+        this.lastname=lname
+    }
+    sayName(){
+        return this.firstname + ' ' + this.lastname
+    }
+   
+}
+const p1=new person('swatantar','verma')
+console.log(p1.sayName())
+
+class superHero extends person{
+    constructor(fname,lname){
+        super(fname,lname)
+        this.issuperHero=true
+    }
+    fightCrime(){
+        console.log('fighting crime')
+    }
+}
+const n=new person('nilu','swatantar')
+console.log(n.sayName())
 
 
 
